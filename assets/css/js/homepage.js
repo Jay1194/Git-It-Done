7eb99886bbbad7b041fca00518bc4df2ad848b45
Jay <j.thomastt1@gmail.com>
2023-08-10 13:44:45 -0600

    Now displays Repository Data

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -4,6 +4,12 @@ var userFormEl = document.querySelector("#user-form");
 // refernce to input element
 var nameInputEl = document.querySelector("#username");
 
+// reference empty div data will be written to
+var repoContainerEl = document.querySelector("#repos-container");
+
+// refernce To username that was searched in form
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 
 // executed upon a form submission browser event
 var formSubmitHandler = function(event) {
@@ -24,10 +30,9 @@ var formSubmitHandler = function(event) {
     // event listener is working correctly
     console.log(event);
 };
-
-
 // submit event listener
 userFormEl.addEventListener("submit", formSubmitHandler);
+
 
 var getUserRepos = function(user) {
     //format the github api url
@@ -35,7 +40,9 @@ var getUserRepos = function(user) {
 
 // make a request to the url
   fetch(apiUrl).then(function(response) {
+    // When the response data is converted to JSON, it will be sent from getUserRepos() to displayRepos()
     response.json().then(function(data) {
+        displayRepos(data, user);
         // repo results
         console.log(data);
     });
@@ -43,6 +50,34 @@ var getUserRepos = function(user) {
 };
 
 
+//Display Repository Data
+// accept both the array of repository data and the term we searched for as parameters
+var displayRepos = function(repos, searchTerm) {
+
+    // clear old content before displaying new content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos (To display on page)
+    for (var i = 0; i< repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo and styles it
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append container (for repo names)
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom (repos)
+        repoContainerEl.appendChild(repoEl);
+    }
+};
 
 
 
