9bfbd54896ea52b12222eb8bd3bc9fa50519a23c
Jay <j.thomastt1@gmail.com>
2023-08-10 11:37:53 -0600

    hard coded formatted response

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -1,6 +1,11 @@
 var getUserRepos = function() {
     //make an HTTP request to /users/octocat/repos on my behalf
-    fetch("https://api.github.com/users/octocat/repos");
-};
+  fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    // formatting response
+  response.json().then(function(data) {
+    console.log(data);
+  });
+});
  
+}
 getUserRepos();
