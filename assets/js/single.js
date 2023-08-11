ad95e2061f486b96fad264ec8b1a991affe8a2a1
Jay <j.thomastt1@gmail.com>
2023-08-11 12:52:27 -0600

    Add an Extra Message for extra issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,9 @@
 //create a reference to the issues container (where to display content)
 var issueContainerEl = document.querySelector("#issues-container");
 
+// api limit warning at bottom of page
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 //list repos issues
 var getRepoIssues = function(repo) {
@@ -12,6 +15,12 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                //check if api has paginated issues (if repo has more then 30 issues) - check for a link header
+                if (response.headers.get("Link")) {
+                    // if true display warning at bototms for link to additional issues
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -20,7 +29,7 @@ var getRepoIssues = function(repo) {
     })
 };
 // hard coded repo to generate issues
-getRepoIssues("Jay1194/portfolio-generator");
+getRepoIssues("facebook/react");
 
 
 // display dom elements on page
@@ -70,4 +79,21 @@ var displayIssues = function(issues) {
         // append <a> element to the actual page
         issueContainerEl.appendChild(issueEl);
     }
+};
+
+// warning indicating there more then 30 issues for that repo 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    // create link to additional issues, that couldint be displayed 
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "see More Issues on GitHub.com";
+    // The actual link with the repo
+    linkEl.setAttribute("href", "https://github.com/"  + repo + "/issues");
+    // send to next page
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
